5e6fa6d098f56a08778a82d8de72de15fcafbe36
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-14 16:40:52 -0500

    Create Excel.py

---
+++ Excel.py
@@ -0,0 +1,24 @@
+'''
+--------------------
+My Excel Program
+--------------------
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print()
+print('Goodbye!')
